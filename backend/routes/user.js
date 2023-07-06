6288237c03ba72c5cc36eede7e8b9bfbc533e0f0
fatihcicek0 <fatihcicek896@gmail.com>
2023-07-06 19:49:45 +0300

    the javascript backend deleted

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authControllers=require('../controllers/auth');
-
-router.post('/register', authControllers.register);
-
-router.post('/login', authControllers.login);
-module.exports = router;
